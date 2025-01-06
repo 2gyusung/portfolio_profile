305210d7deead5828307df38a2e414ae96c19dab
2gyusung <leegs005@naver.com>
2025-01-06 11:12:07 +0900

    swiper 추가적인 수정

--- root/js/swiper.js
+++ root/js/swiper.js
@@ -1,11 +1,19 @@
 
 
       var swiper = new Swiper(".mySwiper", {
-        slidesPerView: 3,
-        spaceBetween: 30,
+        effect: "coverflow",
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: "auto",
+        coverflowEffect: {
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        },
         pagination: {
           el: ".swiper-pagination",
-          clickable: true,
         },
         autoplay: {
       delay: 2500,
