3638c7fe1c3a024bec85b55c70714dd23f193715
2gyusung <leegs005@naver.com>
2025-01-10 11:17:47 +0900

    canvas 기능 추가

---
+++ root/js/canvas.js
@@ -0,0 +1,25 @@
+// do this for 5 seconds
+var duration = 3 * 1000;
+var end = Date.now() + duration;
+
+(function frame() {
+  // launch a few confetti from the left edge
+  confetti({
+    particleCount: 7,
+    angle: 60,
+    spread: 60,
+    origin: { x: 0 }
+  });
+  // and launch a few from the right edge
+  confetti({
+    particleCount: 7,
+    angle: 120,
+    spread: 60,
+    origin: { x: 1 }
+  });
+
+  // keep going until we are out of time
+  if (Date.now() < end) {
+    requestAnimationFrame(frame);
+  }
+})();
