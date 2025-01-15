79d8001575ad0e07dd117e4d7b39dd37a5d05387
2gyusung <leegs005@naver.com>
2025-01-15 10:28:50 +0900

    페이지제작

---
+++ root/js/scrollToTop.js
@@ -0,0 +1,18 @@
+let btn = document.querySelector('.img-btn')
+
+console.log(btn);
+
+window.addEventListener('scroll',()=>{
+  if(window.scrollY> 1) {
+    btn.classList.add('show')
+  }
+  else {
+    btn.classList.remove('show')
+  }
+})
+
+btn.addEventListener('click', ()=> {
+  console.log('클릭!!');
+  
+  window.scrollTo({top: 0, behavior : "smooth"})
+})
