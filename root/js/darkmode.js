fc1ac6b5c2239e24c184ff8c6d6e92029705c323
2gyusung <leegs005@naver.com>
2025-01-21 10:33:21 +0900

    swiper 제거 및 레이아웃 수정

--- root/js/darkmode.js
+++ root/js/darkmode.js
@@ -1,21 +1,11 @@
-const change = document.querySelector("#change");
-const body = document.querySelector("body");
-
-//화면의 배경화면을 전체 바꿔야하니 body 태그를 불러왔다.
-
-function changeHandle () {
-    if(change.value === "NIGHT") {
-        body.classList.add("night");
-        body.classList.remove("day");
-        change.value = "Day";
-        change.style.backgroundColor = "#fff";
-        change.style.color = "#000";
-    } else {
-        body.classList.remove("night");
-        body.classList.add("day");
-        change.value = "NIGHT";
-    }
-}
-
-
-change.addEventListener("click", changeHandle);
+const mode = document.querySelector("#mode");
+mode.addEventListener("click", () => {
+  // 버튼의 텍스트가 다크모드로 만들기 되어 있을 때
+  document.body.classList.toggle("dark-theme");
+  if (mode.textContent == "다크 모드") {
+    mode.textContent = "라이트 모드";
+    // 그렇지 않은 경우 라이트 모드로 되어 있을 경우
+  } else {
+    mode.textContent = "다크 모드";
+  }
+});
--- root/js/scrollToTop.js
+++ root/js/scrollToTop.js
@@ -1,17 +1,15 @@
-let btn = document.querySelector('.img-btn')
+let btn = document.querySelector(".img-btn");
 
-
-window.addEventListener('scroll',()=>{
-  if(window.scrollY> 1) {
-    btn.classList.add('show')
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 1) {
+    btn.classList.add("show");
+  } else {
+    btn.classList.remove("show");
   }
-  else {
-    btn.classList.remove('show')
-  }
-})
+});
 
-btn.addEventListener('click', ()=> {
-  console.log('클릭!!');
-  
-  window.scrollTo({top: 0, behavior : "smooth"})
-})
+btn.addEventListener("click", () => {
+  console.log("클릭!!");
+
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
