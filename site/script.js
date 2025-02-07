572a6b705124dc8ba8d1f0b965faa132de9b832e
이규성 <leegs005@naver.com>
2025-02-07 23:47:34 +0900

    project, 디자인 포토폴리오 자기소개

---
+++ site/script.js
@@ -0,0 +1,23 @@
+const menuIcon = document.querySelector('#menu-icon')
+const navLinks = document.querySelector('.nav-links')
+
+menuIcon.onclick = ()=> {
+  navLinks.classList.toggle('active')
+}
+
+
+let btn = document.querySelector(".img-btn");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 1) {
+    btn.classList.add("show");
+  } else {
+    btn.classList.remove("show");
+  }
+});
+
+btn.addEventListener("click", () => {
+  console.log("클릭!!");
+
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
