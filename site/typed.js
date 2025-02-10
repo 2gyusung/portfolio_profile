6e3d705f59dcf1fcb5784549d94b77d33f85367e
2gyusung <leegs005@naver.com>
2025-02-10 13:21:11 +0900

    수정

--- site/typed.js
+++ site/typed.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".auto-type", {
-  strings : ["Programmer", "Designer", "YouTuber"],
+  strings : ["Passionate", "Gradual", "Growing"],
   typeSpeed : 150,
   backSpeed : 150,
   looped : true
