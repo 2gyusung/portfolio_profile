2c66cfe0b2b195e4e1f6eed07db6d170c7b308e6
2gyusung <leegs005@naver.com>
2025-02-14 16:53:51 +0900

    프로필

--- site/script.js
+++ site/script.js
@@ -7,18 +7,3 @@ menuIcon.onclick = ()=> {
 
 
 let btn = document.querySelector(".img-btn");
-
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 1) {
-    btn.classList.add("show");
-  } else {
-    btn.classList.remove("show");
-  }
-});
-
-btn.addEventListener("click", () => {
-  console.log("클릭!!");
-
-  window.scrollTo({ top: 0, behavior: "smooth" });
-});
-
