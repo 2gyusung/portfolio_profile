96ce8b375bcbf97a3645f5723c2f67f0746fca5e
2gyusung <leegs005@naver.com>
2025-02-20 11:50:30 +0900

    피드백 수정하기

--- site/script.js
+++ site/script.js
@@ -7,7 +7,24 @@ menuIcon.onclick = ()=> {
 
 
 
+// 스크롤 이벤트 기능 
+// gsap.registerPlugin(ScrollTrigger);
 
+
+// gsap.to(".about", {
+//     scrollTrigger: {
+//       trigger: '.about',//객체기준범위
+//       start: "0% 0%",//시작 지점
+//       end: "100% 80%",//끝 지점
+//       // end: "+=500"//시작 부분부터 500px까지 스크롤 한 후종료
+//       scrub: 1,//부드러운 스크러빙
+//       markers: true,//개발가이드선
+//     },
+//     x: 300,
+//     rotation: 360
+//   });
+
+// 프로젝트 모달 기능 
 
 
 // 모든 모달과 버튼을 선택
